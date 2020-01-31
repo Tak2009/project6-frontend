1e87d891481ffa953613ccde1409e8ca44946f56
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-01-31 00:11:00 +0000

    changed

--- src/index.js
+++ src/index.js
@@ -1,20 +1,42 @@
-const btn = document.querySelector("#btn");
-const scriptType = document.querySelector("＃script-type")
+const styleType = document.querySelector("#style-type")
+const scriptType = document.querySelector("#script-type")
+const layout = document.querySelector("#layout")
+const layoutType = document.querySelector("#layout-type")
+
 
 const renderBtns = () => {
-    const btn1 = document.createElement("button")
-    btn1.id = "type1"
-    btn1.innerText = "Layout1"
-    // btn1.addEventListener("click", (e)=> {
-    //     changeLayout(btn)
-    // })
-    btn.appendChild(btn1)
+    const layoutBtn = document.createElement("button")
+    layoutBtn.id = "type1"
+    layoutBtn.innerText = "Change layout"
+    layoutBtn.addEventListener("click", (e)=> {
+        changeLayout(layoutBtn)
+    })
+    layoutType.appendChild(layoutBtn)
 };
 
-// const changeLayout = (btn) => {
-//     btn.className = "script-type1" ? 
+const changeLayout = (layoutBtn) => {
+    if (layoutBtn.id = "type1") {
+        styleType.href = "./stylesheets/style2.css"
+        layoutType.innerHTML = `<div class="row"><div class="col-md-6 bg-left"><h1 class="bg-title">jQuery.BgSwitcher Sample Demo</h1><button id="type2">Change layout</button></div><div class="bg-switcher col-md-6"></div></div>`
+        scriptType.innerHTML = `jQuery(function($) {
+            $('.bg-switcher').bgSwitcher({
+                images: ['https://kishiken.com/common/jq_sample/BgSwitcher/sample1.jpg','https://kishiken.com/common/jq_sample/BgSwitcher/sample2.jpg','https://kishiken.com/common/jq_sample/BgSwitcher/sample3.jpg',"./src/img/test1.jpg","./src/img/test2.jpg"], // 切り替え画像
+                Interval: 5000, //切り替えの間隔 1000=1秒
+                start: true, //$.fn.bgswitcher(config)をコールした時に切り替えを開始する
+                loop: true, //切り替えをループする
+                shuffle: false, //背景画像の順番をシャッフルする
+                effect: "fade", //エフェクトの種類 "fade" "blind" "clip" "slide" "drop" "hide"
+                duration: 1000, //エフェクトの時間 1000=1秒
+                easing: "swing", //エフェクトのイージング "swing" "linear"
+            });
+        });`
+    } else {
+    layoutType.innerHTML = `<div id="layout-type" class="bg-switcher1"><h1 class="bg-title1">BgSwitcher テスト</h1><button id="type1">Change layout</button></div>`
+    }
 
-// }
+      
+       
+}
 
 
 renderBtns()
